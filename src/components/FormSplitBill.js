56487dbe08df4ab9cf29ed041444ee3779f45b07
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-01 20:43:26 -0600

    getting inputs info in FormSplitBill

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,29 +1,62 @@
+import { useState } from 'react';
 import Button from './Button';
 
 export default function FormSplitBill({ selectedFriend }) {
+  const [billValue, setBillValue] = useState('');
+  const [yourExpenses, setYourExpenses] = useState('');
+  const [friendExpenses, setFriendExpenses] = useState('');
+  const [paysBill, setPaysBill] = useState('');
+
   function formHandler(e) {
     e.preventDefault();
+    console.log('!');
   }
+
   return (
-    <form className='form-split-bill'>
+    <form className='form-split-bill' onSubmit={formHandler}>
       <h2>Split a bill with {selectedFriend?.name}</h2>
 
       <label>Bill value</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={billValue}
+        onChange={(e) => {
+          console.log(billValue);
+          setBillValue(e.target.value);
+        }}
+      />
 
       <label>Your expenses</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={yourExpenses}
+        onChange={(e) => {
+          setYourExpenses(e.target.value);
+        }}
+      />
 
       <label>{selectedFriend?.name} expenses</label>
-      <input type='text' disabled />
+      <input
+        type='text'
+        value={friendExpenses}
+        onChange={(e) => {
+          setFriendExpenses(e.target.value);
+        }}
+        disabled
+      />
 
       <label>Who is paying the bill</label>
-      <select>
+      <select
+        value={paysBill}
+        onChange={(e) => {
+          setPaysBill(e.target.value);
+        }}
+      >
         <option value='user'>You</option>
         <option value='friend'>{selectedFriend?.name}</option>
       </select>
 
-      <Button onClick={formHandler}>Split bill</Button>
+      <Button>Split bill</Button>
     </form>
   );
 }
