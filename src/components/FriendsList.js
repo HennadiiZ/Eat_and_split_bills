9e8c537a4b6bb09e214294bc1daf847640373a3e
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-04 11:15:54 -0600

    cleaning up comments

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,36 +1,3 @@
-// // import { useState } from 'react';
-// import Friend from './Friend';
-
-// export default function FriendsList({
-//   initialFriends,
-//   selectedFriend,
-//   setSelectedFriend,
-// }) {
-//   function selectAfriendHandler(id) {
-//     initialFriends.filter((friend) => {
-//       if (friend.id === id) {
-//         setSelectedFriend(friend);
-//       }
-//     });
-//   }
-
-//   return (
-//     <ul>
-//       {initialFriends.map((item) => (
-//         <Friend
-//           item={item}
-//           key={item.id}
-//           selectedFriend={selectedFriend}
-//           setSelectedFriend={setSelectedFriend}
-//           selectAfriendHandler={selectAfriendHandler}
-//         />
-//       ))}
-//     </ul>
-//   );
-// }
-//
-//------------------------------------------------------------------
-
 import Friend from './Friend';
 
 export default function FriendsList({
@@ -68,34 +35,3 @@ export default function FriendsList({
     </ul>
   );
 }
-//
-// function Friend({
-//   item,
-//   selectedFriend,
-//   setSelectedFriend,
-//   selectAfriendHandler,
-// }) {
-//   const isSelected = selectedFriend?.id === item?.id;
-
-//   return (
-//     <li className={isSelected ? 'selected' : ''}>
-//       <img src={item.image} alt={item.name}></img>
-//       <h3>{item.name}</h3>
-
-//       {item.balance < 0 && (
-//         <p className='red'>
-//           You owe {item.name} ${Math.abs(item.balance)}
-//         </p>
-//       )}
-//       {item.balance > 0 && (
-//         <p className='green'>
-//           {item.name} owes you ${Math.abs(item.balance)}
-//         </p>
-//       )}
-//       {item.balance === 0 && <p>You and {item.name} are even</p>}
-//       <Button click={() => selectAfriendHandler(item.id)}>
-//         {isSelected ? 'Close' : 'Select'}
-//       </Button>
-//     </li>
-//   );
-// }
